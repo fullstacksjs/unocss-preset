b2cb620d0b0a358835b7cf8e1c94a5cdbec5b9ee
Alireza Safaierad <frontendmonster@gmail.com>
2025-04-27 01:53:51 +0200

    chore: add tsdown

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,10 @@ import { defineConfig } from '@fullstacksjs/eslint-config';
 export default defineConfig({
   strict: true,
   node: true,
+  typescript: {
+    tsconfigRootDir: import.meta.dirname,
+    projectService: {
+      allowDefaultProject: ['tsdown.config.ts'],
+    },
+  },
 });
